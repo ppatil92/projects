f33c313eb3dfa83e37233d2a2a9ffd32cdbdb5c6
perpetualLearner92 <c4.prathamesh.patil@gmail.com>
2021-08-12 15:49:28 +0100

    removing unused import and dependency

--- demo/src/test/java/com/accela/interview/demo/service/AddressServiceTest.java
+++ demo/src/test/java/com/accela/interview/demo/service/AddressServiceTest.java
@@ -2,17 +2,12 @@ package com.accela.interview.demo.service;
 
 import com.accela.interview.demo.entity.Person;
 import com.accela.interview.demo.repository.AddressRepository;
-import com.accela.interview.demo.service.AddressService;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
